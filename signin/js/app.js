634cd7f8a17b2d21844d5b7da79018978450b0a2
LocaZika <buinhupha@gmail.com>
2022-09-25 16:16:42 +0700

    signin-form update

--- signin/js/app.js
+++ signin/js/app.js
@@ -1,12 +1,62 @@
 const $ = document.querySelector.bind(document);
 const signInForm = $(".signin-form");
 const signUpForm = $(".signup-form");
-const signUpButton = $(".signup > button");
+const signUpButton1 = $("#btn-signup-form1");
+const signUpButton2 = $("#btn-signup-form2");
+const signUpButton3 = $("#btn-signup-form3");
 
-signUpButton.addEventListener("click", () => {
-  signInForm.style.transformOrigin = "left";
-  signInForm.style.transform = "perspective(1200px) rotateY(20deg)";
+/* OPEN DOOR */
+signUpButton1.addEventListener("click", () => {
+  signInForm.style = "";
+  signUpForm.style = "";
   setTimeout(() => {
-    signInForm.style = "";
+    signInForm.style.transformOrigin = "left";
+    signInForm.style.transform = "perspective(1200px) rotateY(20deg)";
+    setTimeout(() => {
+      signInForm.style = "";
+    }, 1000);
+    signUpForm.style.transformOrigin = "left";
+    signUpForm.style.transform = "perspective(1200px) rotateY(20deg)";
+    signUpForm.style.left = "0";
+    setTimeout(() => {
+      signUpForm.style.left = "0";
+      signUpForm.style.transform = "perspective(1200px) rotateY(0deg)";
+    }, 1000);
+  }, 1000);
+});
+/* MOVE TOP AND LEFT */
+signUpButton2.addEventListener("click", () => {
+  signInForm.style = "";
+  signUpForm.style = "";
+  setTimeout(() => {
+    signInForm.style.transformOrigin = "left";
+    signInForm.style.transform = "perspective(1200px) rotateY(20deg)";
+    setTimeout(() => {
+      signInForm.style = "";
+    }, 1500);
+    setTimeout(() => {
+      signUpForm.style.left = "0";
+      signUpForm.style.transform = "perspective(1200px) rotateY(0deg)";
+      signUpForm.style.zIndex = "3";
+      setTimeout(() => {
+        signUpForm.style.left = "50%";
+      }, 500);
+    }, 1000);
+  }, 1000);
+});
+/* TURN THE PAGE  */
+signUpButton3.addEventListener("click", () => {
+  signInForm.style = "";
+  signUpForm.style = "";
+  setTimeout(() => {
+    signUpForm.style.transformOrigin = "right";
+    signUpForm.style.transform = "perspective(1200px) rotateY(180deg)";
+    setTimeout(() => {
+      setTimeout(() => {
+        signUpForm.style.transformOrigin = "left";
+        signUpForm.style.transform = "perspective(1200px) rotateY(360deg)";
+      }, 100);
+      signUpForm.style.zIndex = "3";
+    }, 500);
   }, 1000);
 });
