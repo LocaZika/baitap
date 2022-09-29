12fd45dc1c4bf7bac9757655fde9c0f9fcb8727d
LocaZika <buinhupha@gmail.com>
2022-09-29 18:07:54 +0700

    add menuRes

---
+++ menuRes/js/app.js
@@ -0,0 +1,6 @@
+const $ = document.querySelector.bind(document);
+const navbar = $(".navbar");
+const bar = $("#bar");
+const barLineTop = $("#bar__line--top");
+const barLineMid = $("#bar__line--mid");
+const barLineBottom = $("#bar__line--bottom");
