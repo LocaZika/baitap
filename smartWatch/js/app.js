3f13e7dcdbdb1d82adf777ab9e2a21e7a707ec9a
LocaZika <buinhupha@gmail.com>
2022-10-14 02:15:44 +0700

    init smartwatch pj

---
+++ smartWatch/js/app.js
@@ -0,0 +1,18 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const resizeObs = new ResizeObserver(() => {
+  $("#bg").style.height = $("header").offsetHeight + $(".hiw").offsetHeight / 3 + "px";
+});
+resizeObs.observe($("header"));
+const questions = $$(".faq .questions-grp .question");
+for (const index in questions) {
+  if (Object.hasOwnProperty.call(questions, index)) {
+    const question = questions[index];
+    question.addEventListener("click", () => {
+      question.classList.toggle("active");
+    });
+  }
+}
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 5220) $(".cta").classList.add("scroll");
+});
