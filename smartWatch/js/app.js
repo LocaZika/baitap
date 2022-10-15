0c5c71f004d822e735196b4ae382c78b95b3455e
LocaZika <60792232+LocaZika@users.noreply.github.com>
2022-10-16 01:05:41 +0700

    update pj

--- smartWatch/js/app.js
+++ smartWatch/js/app.js
@@ -1,7 +1,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const resizeObs = new ResizeObserver(() => {
-  $("#bg").style.height = $("header").offsetHeight + $(".hiw").offsetHeight / 3 + "px";
+  $("#bg").style = `height: ${$("header").offsetHeight + "px"};
+   top: 70%;`;
 });
 resizeObs.observe($("header"));
 const questions = $$(".faq .questions-grp .question");
@@ -15,4 +16,5 @@ for (const index in questions) {
 }
 window.addEventListener("scroll", () => {
   if (window.scrollY > 5110) $(".cta").classList.add("scroll");
+  if (window.scrollY > 5500 || window.scrollY < 4500) $(".cta").classList.remove("scroll");
 });
