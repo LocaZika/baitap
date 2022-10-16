0d0c49822a94dd721fb1babeca6dcd63b38eebcd
LocaZika <60792232+LocaZika@users.noreply.github.com>
2022-10-16 17:29:55 +0700

    last change before change scs

--- smartWatch/js/app.js
+++ smartWatch/js/app.js
@@ -1,10 +1,12 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+/*===CHANGE #bg HEIGHT===*/
 const resizeObs = new ResizeObserver(() => {
   $("#bg").style = `height: ${$("header").offsetHeight + "px"};
    top: 70%;`;
 });
 resizeObs.observe($("header"));
+/*===FAQ SHOW/HIDE===*/
 const questions = $$(".faq .questions-grp .question");
 for (const index in questions) {
   if (Object.hasOwnProperty.call(questions, index)) {
@@ -14,7 +16,13 @@ for (const index in questions) {
     });
   }
 }
+/*===CTA ANIMATE===*/
 window.addEventListener("scroll", () => {
   if (window.scrollY > 5110) $(".cta").classList.add("scroll");
   if (window.scrollY > 5500 || window.scrollY < 4500) $(".cta").classList.remove("scroll");
 });
+/*===SCREENSHOTS CHANGE ORDER===*/
+// const scsImgs = $$(".scs-img label");
+// let isChecked = false;
+const scsImgs = $(".scs-imgs");
+const scsControls = $$(".scs-control input[type='radio']");
